818caec40fc3bdda2379c23b7bd92ea20331c5b6
Christodoulos Fragkoudakis <chfrag@mail.ntua.gr>
2020-02-23 19:39:12 +0200

    Moved example.py in examples

--- examples/example.py
+++ examples/example.py
@@ -7,9 +7,7 @@ from ucc2stl import dense_cuboids, CuboidComplex
 # from utils import dense_cuboids
 # from cuboids import CuboidComplex
 
-CUBOIDS = dense_cuboids(
-    "examples/Node.txt", "examples/Connectivity.txt", "examples/density.txt", 0.3
-)
+CUBOIDS = dense_cuboids("Node.txt", "Connectivity.txt", "density.txt", 0.3)
 CUBOID_COMPLEX = CuboidComplex(CUBOIDS)
 CUBOID_COMPLEX.export_off()
 CUBOID_COMPLEX.export_stl()
